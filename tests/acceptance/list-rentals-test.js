dfa341b2bd5a369b034795e9d5b00202203046f3
Danny Evins <dcastkid@gmail.com>
2016-07-18 22:00:14 -0500

    add app template

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -7,15 +7,15 @@ test('should list available rentals.', function (assert) {
   visit('/');
   andThen(function () {
     assert.equal(find('.listing').length, 3, 'should see 3 listings');
-  })
+  });
 });
 
 test('should link to information about the company.', function (assert) {
   visit('/');
   click('a:contains("About")');
   andThen(function () {
-    assert.equal(currentURL(), '/about', 'should navigate to about')
-  })
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
 });
 
 test('should link to contact information.', function (assert) {
